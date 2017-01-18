bd75f14cf7c8c592b1389d20801c458cd55ef0c1
David Tittle <drtittle@us.ibm.com>
2017-01-18 12:04:54 -0600

    Using Express Router.

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 // This if for ICAL
 
@@ -34,7 +36,7 @@ app.use(express.static(__dirname + '/public'));
 
 // ROUTES FOR OUR API
 // =============================================================================
-//var router = express.Router();              // get an instance of the express Router
+var router = express.Router(); // get an instance of the express Router
 
 // middleware to use for all requests
 //router.use(function(req, res, next) {
@@ -54,50 +56,45 @@ var ical = require('./node_modules/ical/node-ical');
 
 // on routes that end in /ical
 //router.route('/ical')
-    
+
 // on routes that end in /ical/:icalUrl
-//router.route('/ical/:icalUrl')
-//
-//    // get the ical 
-//    .get(function(req, res) {    
- 
-    
-app.get('/ical/:icalUrl', function(req, res) {
-	       
-        var list = new Array();
-		var url = req.params.bear_id || 'https://calendar.google.com/calendar/ical/o8mfhn5drq7t875vosh3b5kdao%40group.calendar.google.com/public/basic.ics';
+router.route('/ical/:icalUrl').get(function(req, res) {
+	//app.get('/ical/:icalUrl', function(req, res) {
 
-		ical.fromURL(url, {}, function(err, data) {
-			if (err) {
-				res.send(err);
-			} else {
-				for (var k in data) {
-					if (data.hasOwnProperty(k)) {
-						var ev = data[k];
+	var list = new Array();
+	var url = req.params.bear_id || 'https://calendar.google.com/calendar/ical/o8mfhn5drq7t875vosh3b5kdao%40group.calendar.google.com/public/basic.ics';
 
-						var arr = [{
-							'summary': ev.summary,
-							'start': ev.start,
-							'end': ev.end
-						}];
-						//console.log("get the row",ev.summary,ev.start,ev.end) -- fetching correct output thou it has duplicates
-						//list.add(arr);
-						list.push(arr);
+	ical.fromURL(url, {}, function(err, data) {
+		if (err) {
+			res.send(err);
+		} else {
+			for (var k in data) {
+				if (data.hasOwnProperty(k)) {
+					var ev = data[k];
 
-						// ArrayList is not working
-						//console.log("ArrayList_nowthea", arr);
-					}
+					var arr = [{
+						'summary': ev.summary,
+						'start': ev.start,
+						'end': ev.end
+					}];
+					//console.log("get the row",ev.summary,ev.start,ev.end) -- fetching correct output thou it has duplicates
+					//list.add(arr);
+					list.push(arr);
+
+					// ArrayList is not working
+					//console.log("ArrayList_nowthea", arr);
 				}
+			}
 			/* arraytob(list);
                       console.log("theanmozhi arraytob",arraytob);*/
-                     
-                res.json(list);
-			}
 
-		});
-		res.json(list);
-		//return arr;
-    });
+			res.json(list);
+		}
+
+	});
+	res.json(list);
+	//return arr;
+});
 
 
 
@@ -110,6 +107,6 @@ var appEnv = cfenv.getAppEnv();
 // start server on the specified port and binding host
 //app.listen(appEnv.port, '0.0.0.0', function() {
 var server = app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url + ":" + appEnv.port);
+	// print a message when the server starts listening
+	console.log("server starting on " + appEnv.url + ":" + appEnv.port);
 });
